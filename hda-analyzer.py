79a211a29916556352237725c142d41638129380
Esteban Jatuff <estebanjatuff@gmail.com>
2025-11-14 15:45:58 -0300

    probando

---
+++ hda-analyzer.py
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<!-- git web interface version 1.8.3.1, (C) 2005-2006, Kay Sievers <kay.sievers@vrfy.org>, Christian Gierke -->
+<!-- git core binaries version 1.8.3.1 -->
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="generator" content="gitweb/1.8.3.1 git/1.8.3.1"/>
+<meta name="robots" content="index, nofollow"/>
+<title>git.alsa-project.org Git - alsa.git/summary</title>
+<link rel="stylesheet" type="text/css" href="static/gitweb.css"/>
+<link rel="alternate" title="alsa.git - log - RSS feed" href="/?p=alsa.git;a=rss" type="application/rss+xml" />
+<link rel="alternate" title="alsa.git - log - RSS feed (no merges)" href="/?p=alsa.git;a=rss;opt=--no-merges" type="application/rss+xml" />
+<link rel="alternate" title="alsa.git - log - Atom feed" href="/?p=alsa.git;a=atom" type="application/atom+xml" />
+<link rel="alternate" title="alsa.git - log - Atom feed (no merges)" href="/?p=alsa.git;a=atom;opt=--no-merges" type="application/atom+xml" />
+<link rel="shortcut icon" href="static/git-favicon.png" type="image/png" />
+</head>
+<body>
+<div class="page_header">
+<a title="git homepage" href="http://git-scm.com/"><img class="logo" height="27" src="static/git-logo.png" alt="git" width="72" /></a><a href="/">git://git.alsa-project.org</a> / <a href="/?p=alsa.git;a=summary">alsa.git</a> / summary
+</div>
+<form method="get" action="/" enctype="application/x-www-form-urlencoded"><div class="search">
+<input name="p" type="hidden" value="alsa.git" />
+<input name="a" type="hidden" value="search" />
+<input name="h" type="hidden" value="HEAD" />
+<select name="st" >
+<option selected="selected" value="commit">commit</option>
+<option value="grep">grep</option>
+<option value="author">author</option>
+<option value="committer">committer</option>
+<option value="pickaxe">pickaxe</option>
+</select><sup><a href="/?p=alsa.git;a=search_help">?</a></sup> search:
+<input type="text" name="s"  />
+<span title="Extended regular expression"><label><input type="checkbox" name="sr" value="1" />re</label></span></div>
+</form>
+<div class="page_nav">
+summary | <a href="/?p=alsa.git;a=shortlog">shortlog</a> | <a href="/?p=alsa.git;a=log">log</a> | <a href="/?p=alsa.git;a=commit;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b">commitdiff</a> | <a href="/?p=alsa.git;a=tree">tree</a><br/>
+<br/>
+</div>
+<div class="title">&nbsp;</div>
+<table class="projects_list">
+<tr id="metadata_desc"><td>description</td><td>ALSA repository for misc stuff (service like)</td></tr>
+<tr id="metadata_owner"><td>owner</td><td>GIT server</td></tr>
+<tr id="metadata_lchange"><td>last change</td><td><span class="datetime">Fri, 15 Nov 2019 12:47:52 +0000</span> (13:47 +0100)</td></tr>
+</table>
+<div class="header">
+<a class="title" href="/?p=alsa.git;a=shortlog">shortlog</a>
+</div>
+<table class="shortlog">
+<tr class="dark">
+<td title="2 years ago"><i>2019-11-15</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b">updates</a> <span class="refs"> <span class="head" title="heads/master"><a href="/?p=alsa.git;a=shortlog;h=refs/heads/master">master</a></span></span></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b;hb=dd0f37a66ad3461b29299be37c3f1791098c2c9b">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=dd0f37a66ad3461b29299be37c3f1791098c2c9b;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="4 years ago"><i>2017-10-19</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=55498f528103ff3bdeece3f1c861f96c05bb3454">hda-analyzer: ignore '*Dev' lines</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=55498f528103ff3bdeece3f1c861f96c05bb3454">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=55498f528103ff3bdeece3f1c861f96c05bb3454">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=55498f528103ff3bdeece3f1c861f96c05bb3454;hb=55498f528103ff3bdeece3f1c861f96c05bb3454">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=55498f528103ff3bdeece3f1c861f96c05bb3454;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="6 years ago"><i>2015-09-03</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=b945b2f0590c7b13397aa9dea79a112224f4f141">hda-analyzer: fix for recent driver</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=b945b2f0590c7b13397aa9dea79a112224f4f141">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=b945b2f0590c7b13397aa9dea79a112224f4f141">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=b945b2f0590c7b13397aa9dea79a112224f4f141;hb=b945b2f0590c7b13397aa9dea79a112224f4f141">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=b945b2f0590c7b13397aa9dea79a112224f4f141;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="7 years ago"><i>2014-07-30</i></td>
+<td class="author"><a title="Search for commits authored by Takashi Iwai" class="list" href="/?p=alsa.git;a=search;s=Takashi+Iwai;st=author">Takashi Iwai</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=48ed9474fc6724dc1486d30e9a274ab27ec001da">hda_analyzer: Parse &quot;KAE&quot; in Digital line</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=48ed9474fc6724dc1486d30e9a274ab27ec001da">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=48ed9474fc6724dc1486d30e9a274ab27ec001da">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=48ed9474fc6724dc1486d30e9a274ab27ec001da;hb=48ed9474fc6724dc1486d30e9a274ab27ec001da">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=48ed9474fc6724dc1486d30e9a274ab27ec001da;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="7 years ago"><i>2014-07-30</i></td>
+<td class="author"><a title="Search for commits authored by Takashi Iwai" class="list" href="/?p=alsa.git;a=search;s=Takashi+Iwai;st=author">Takashi Iwai</a></td><td><a class="list subject" title="hda_analyzer: Add &quot;In-driver Connection&quot; and &quot;Devices&quot; lines to parser" href="/?p=alsa.git;a=commit;h=56c640a98975b03a6a78a44b2c01e49dfd7b5263">hda_analyzer: Add &quot;In-driver Connection&quot; and &quot;Devices... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=56c640a98975b03a6a78a44b2c01e49dfd7b5263">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=56c640a98975b03a6a78a44b2c01e49dfd7b5263">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=56c640a98975b03a6a78a44b2c01e49dfd7b5263;hb=56c640a98975b03a6a78a44b2c01e49dfd7b5263">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=56c640a98975b03a6a78a44b2c01e49dfd7b5263;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="9 years ago"><i>2013-05-30</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" title="hda_analyzer: don't fail to display audio selector if caps variables aren't available" href="/?p=alsa.git;a=commit;h=5bcf62d6947fe348a2b055bce633df1d12ff4370">hda_analyzer: don't fail to display audio selector... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=5bcf62d6947fe348a2b055bce633df1d12ff4370">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=5bcf62d6947fe348a2b055bce633df1d12ff4370">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=5bcf62d6947fe348a2b055bce633df1d12ff4370;hb=5bcf62d6947fe348a2b055bce633df1d12ff4370">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=5bcf62d6947fe348a2b055bce633df1d12ff4370;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="9 years ago"><i>2013-05-04</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" title="hda_analyzer: Remove the duplicated bits introduced at the wrong indentation level." href="/?p=alsa.git;a=commit;h=0909633288777d27c67aee2a358218a6434f181a">hda_analyzer: Remove the duplicated bits introduced... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=0909633288777d27c67aee2a358218a6434f181a">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=0909633288777d27c67aee2a358218a6434f181a">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=0909633288777d27c67aee2a358218a6434f181a;hb=0909633288777d27c67aee2a358218a6434f181a">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=0909633288777d27c67aee2a358218a6434f181a;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="9 years ago"><i>2013-04-29</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=f949105872e87825deba126b5aaf8d2193986621">hda_analyzer: Fix len(None) backtrace</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=f949105872e87825deba126b5aaf8d2193986621">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=f949105872e87825deba126b5aaf8d2193986621">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=f949105872e87825deba126b5aaf8d2193986621;hb=f949105872e87825deba126b5aaf8d2193986621">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=f949105872e87825deba126b5aaf8d2193986621;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="9 years ago"><i>2013-01-28</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=3a9adf8e08ace742a9ed5fd3e116e4052803a953">fft1.py: remove size-800 pattern</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=3a9adf8e08ace742a9ed5fd3e116e4052803a953">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=3a9adf8e08ace742a9ed5fd3e116e4052803a953">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=3a9adf8e08ace742a9ed5fd3e116e4052803a953;hb=3a9adf8e08ace742a9ed5fd3e116e4052803a953">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=3a9adf8e08ace742a9ed5fd3e116e4052803a953;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="9 years ago"><i>2013-01-22</i></td>
+<td class="author"><a title="Search for commits authored by Takashi Iwai" class="list" href="/?p=alsa.git;a=search;s=Takashi+Iwai;st=author">Takashi Iwai</a></td><td><a class="list subject" title="hda_analyzer: Fix parser error with recently updated proc files" href="/?p=alsa.git;a=commit;h=7859cb050f2cbeba54ce46e3f4cac810f60bd523">hda_analyzer: Fix parser error with recently updated... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=7859cb050f2cbeba54ce46e3f4cac810f60bd523">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=7859cb050f2cbeba54ce46e3f4cac810f60bd523">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=7859cb050f2cbeba54ce46e3f4cac810f60bd523;hb=7859cb050f2cbeba54ce46e3f4cac810f60bd523">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=7859cb050f2cbeba54ce46e3f4cac810f60bd523;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="9 years ago"><i>2012-09-06</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" title="alsatool: alsa-driver merging changes, comments.py updated" href="/?p=alsa.git;a=commit;h=3630f7817679f29f77f53a6ef75828f4f106c2fd">alsatool: alsa-driver merging changes, comments.py... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=3630f7817679f29f77f53a6ef75828f4f106c2fd">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=3630f7817679f29f77f53a6ef75828f4f106c2fd">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=3630f7817679f29f77f53a6ef75828f4f106c2fd;hb=3630f7817679f29f77f53a6ef75828f4f106c2fd">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=3630f7817679f29f77f53a6ef75828f4f106c2fd;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="9 years ago"><i>2012-08-20</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=1a2c21e4d3816dfdbb467c0839542a011843b807">Added -right option to fft1.py</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=1a2c21e4d3816dfdbb467c0839542a011843b807">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=1a2c21e4d3816dfdbb467c0839542a011843b807">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=1a2c21e4d3816dfdbb467c0839542a011843b807;hb=1a2c21e4d3816dfdbb467c0839542a011843b807">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=1a2c21e4d3816dfdbb467c0839542a011843b807;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="9 years ago"><i>2012-08-20</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=74fa2c78bdfba2380968b223d8bb4da939816ef9">Added comment to fft1.py</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=74fa2c78bdfba2380968b223d8bb4da939816ef9">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=74fa2c78bdfba2380968b223d8bb4da939816ef9">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=74fa2c78bdfba2380968b223d8bb4da939816ef9;hb=74fa2c78bdfba2380968b223d8bb4da939816ef9">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=74fa2c78bdfba2380968b223d8bb4da939816ef9;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="9 years ago"><i>2012-08-20</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=e25d5ff349ee170d5b73e1e4de76958f6c3654fc">Added fft1.py test</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=e25d5ff349ee170d5b73e1e4de76958f6c3654fc">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=e25d5ff349ee170d5b73e1e4de76958f6c3654fc">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=e25d5ff349ee170d5b73e1e4de76958f6c3654fc;hb=e25d5ff349ee170d5b73e1e4de76958f6c3654fc">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=e25d5ff349ee170d5b73e1e4de76958f6c3654fc;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="dark">
+<td title="23 years ago"><i>1998-08-13</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" href="/?p=alsa.git;a=commit;h=83e9251c00f8c6be2ccc0f78965f85fa585db14b">hda-analyzer: allow more rendering options</a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=83e9251c00f8c6be2ccc0f78965f85fa585db14b">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=83e9251c00f8c6be2ccc0f78965f85fa585db14b">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=83e9251c00f8c6be2ccc0f78965f85fa585db14b;hb=83e9251c00f8c6be2ccc0f78965f85fa585db14b">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=83e9251c00f8c6be2ccc0f78965f85fa585db14b;sf=tgz">snapshot</a></td>
+</tr>
+<tr class="light">
+<td title="10 years ago"><i>2012-05-02</i></td>
+<td class="author"><a title="Search for commits authored by Jaroslav Kysela" class="list" href="/?p=alsa.git;a=search;s=Jaroslav+Kysela;st=author">Jaroslav Kysela</a></td><td><a class="list subject" title="hda_analyzer: Fix mixer controls using pcm or card interface id" href="/?p=alsa.git;a=commit;h=71f3a40f3b59293bb09b73a20b375311fcf6e562">hda_analyzer: Fix mixer controls using pcm or card... </a></td>
+<td class="link"><a href="/?p=alsa.git;a=commit;h=71f3a40f3b59293bb09b73a20b375311fcf6e562">commit</a> | <a href="/?p=alsa.git;a=commitdiff;h=71f3a40f3b59293bb09b73a20b375311fcf6e562">commitdiff</a> | <a href="/?p=alsa.git;a=tree;h=71f3a40f3b59293bb09b73a20b375311fcf6e562;hb=71f3a40f3b59293bb09b73a20b375311fcf6e562">tree</a> | <a title="in format: tar.gz" href="/?p=alsa.git;a=snapshot;h=71f3a40f3b59293bb09b73a20b375311fcf6e562;sf=tgz">snapshot</a></td>
+</tr>
+<tr>
+<td colspan="4"><a href="/?p=alsa.git;a=shortlog">...</a></td>
+</tr>
+</table>
+<div class="header">
+<a class="title" href="/?p=alsa.git;a=heads">heads</a>
+</div>
+<table class="heads">
+<tr class="dark">
+<td><i>2 years ago</i></td>
+<td class="current_head"><a class="list name" href="/?p=alsa.git;a=shortlog;h=refs/heads/master">master</a></td>
+<td class="link"><a href="/?p=alsa.git;a=shortlog;h=refs/heads/master">shortlog</a> | <a href="/?p=alsa.git;a=log;h=refs/heads/master">log</a> | <a href="/?p=alsa.git;a=tree;h=refs/heads/master;hb=refs/heads/master">tree</a></td>
+</tr></table>
+<div class="page_footer">
+<div class="page_footer_text">ALSA repository for misc stuff (service like)</div>
+<a class="rss_logo" title="log RSS feed" href="/?p=alsa.git;a=rss">RSS</a>
+<a class="rss_logo" title="log Atom feed" href="/?p=alsa.git;a=atom">Atom</a>
+</div>
+<script type="text/javascript" src="static/gitweb.js"></script>
+<script type="text/javascript">
+window.onload = function () {
+	var tz_cookie = { name: 'gitweb_tz', expires: 14, path: '/' };
+	onloadTZSetup('local', tz_cookie, 'datetime');
+};
+</script>
+</body>
+</html>
